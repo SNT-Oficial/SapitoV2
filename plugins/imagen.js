048b4575013d209d40b45f490fa71ec552c96da2
Ⓢ︎Ⓝ︎Ⓣ︎ <120823387+SNT-Oficial@users.noreply.github.com>
2022-12-18 00:14:36 -0300

    Update imagen.js

--- plugins/imagen.js
+++ plugins/imagen.js
@@ -7,9 +7,7 @@ let results = await gis(text) || []
 let { url, width, height } = pickRandom(results) || {}
 if (!url) return m.reply('*[❗] 𝚂𝑒𝑟𝑣𝑖𝑑𝑜𝑟 𝑐𝑎𝑖𝑑𝑜*')
 conn.sendFile(m.chat, url, 'gimage', `
-🔎 *𝐴𝑞𝑢𝑖 𝑡𝑖𝑒𝑛𝑒𝑠 𝑒𝑙 𝑟𝑒𝑠𝑢𝑙𝑡𝑎𝑑𝑜 𝑑𝑒* 
-         ${text}
-_sapito-Bot_
+Por favor para yaaa 😩
 `.trim(), m)}
 handler.help = ['gimage <query>', 'image <query>']
 handler.tags = ['general']
