170f86807fbdd00b5c74b9258521254b64ebec96
Ⓢ︎Ⓝ︎Ⓣ︎ <120823387+SNT-Oficial@users.noreply.github.com>
2022-12-17 23:50:27 -0300

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -1,45 +1,42 @@
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
-let { MessageType } = require('@adiwajshing/baileys')
-let yts = require('yt-search')
-let fs = require('fs')
-let fetch = require('node-fetch')
-const { servers, yta, ytv } = require('../lib/y2mate')
-let handler = async(m, { conn, command, text, isPrems, isOwner, DevMode, args, usedPrexi }) => {
-conn.play = conn.play ? conn.play : {}
-if (!text) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝑵𝒐𝒎𝒃𝒓𝒆 𝒅𝒆 𝒍𝒂 𝒄𝒂𝒏𝒄𝒊𝒐𝒏 𝒇𝒂𝒍𝒕𝒂𝒏𝒕𝒆, 𝑷𝒐𝒓 𝒇𝒂𝒗𝒐𝒓 𝒊𝒏𝒈𝒓𝒆𝒔𝒆 𝒆𝒍 𝒄𝒐𝒎𝒂𝒏𝒅𝒐 𝒎𝒂𝒔 𝒆𝒍 𝒏𝒐𝒎𝒃𝒓𝒆/𝑻𝒊𝒕𝒖𝒍𝒐 𝒅𝒆 𝒖𝒏𝒂 𝒄𝒂𝒏𝒄𝒊𝒐𝒏*\n\n*—◉ 𝒆𝒋𝒆𝒎𝒑𝒍𝒐:*\n*#play Los meketrefes hoy te vi*'
+import { youtubeSearch } from '@bochilteam/scraper'
+import fetch from 'node-fetch'
+let handler = async (m, { conn, command, text, usedPrefix }) => {
+if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙻𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽 𝙵𝙰𝙻𝚃𝙰𝙽𝚃𝙴, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝙼𝙰𝚂 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴/𝚃𝙸𝚃𝚄𝙻𝙾 𝙳𝙴 𝚄𝙽𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽*\n\n*—◉ 𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*${usedPrefix + command} Good Feeling - Flo Rida*`
 try {
-let results = await yts(text)
-let vid = results.all.find(video => video.seconds < 3600)
-let { dl_link, thumb, title, filesize, filesizeF } = await (/2$/.test(command) ? ytv : yta)(vid.url, 'id4')
-let shortUrl = await (await fetch(`https://tinyurl.com/api-create.php?url=${dl_link}`)).text()
-conn.sendFile(m.chat, thumb, 'error.jpg', `
-📌 *𝑇𝑖𝑡𝑢𝑙𝑜:* ${title}
-*📁 𝑃𝑒𝑠𝑜:* ${filesizeF}
-*🔗 𝑈𝑅𝐿:* ${vid.url}
-*📥 𝐷𝑒𝑠𝑐𝑎𝑟𝑔𝑎𝑟:* ${shortUrl}
-`.trim(), m, false, { 
-contextInfo: { externalAdReply: {
-title: 'ʀᴇᴘʀᴏᴅᴜᴄᴛᴏʀ ᴅᴇ ʏᴏᴜᴛᴜʙᴇ',
-body: '©𝑆𝑎𝑝𝑖𝑡𝑜𝑏𝑜𝑡', 
-sourceUrl: `https://github.com/BrunoSobrino/SimpleBot`, 
-thumbnail: fs.readFileSync('./Menu2.jpg') }}})
-conn.sendFile(m.chat, dl_link, title + '.mp' + (3 + /2$/.test(command)), `
-*📌 𝑇𝑖𝑡𝑢𝑙𝑜:* ${title}
-*📁 𝑃𝑒𝑠𝑜:* ${filesizeF}
-*🔗 𝑈𝑅𝐿:* ${vid.url}
-`.trim(), m)
+let vid = (await youtubeSearch(text)).video[0]
+let { title, description, thumbnail, videoId, durationH, viewH, publishedTime } = vid
+const urll = 'https://www.youtube.com/watch?v=' + videoId
+var doc = ['pdf','zip','vnd.openxmlformats-officedocument.presentationml.presentation','vnd.openxmlformats-officedocument.spreadsheetml.sheet','vnd.openxmlformats-officedocument.wordprocessingml.document']
+var document = doc[Math.floor(Math.random() * doc.length)]
+const buttons = [
+{ buttonId: `#ytmp3 ${urll}`, buttonText: { displayText: '🎵 𝐀𝐔𝐃𝐈𝐎 🎵' }, type: 1 },
+{ buttonId: `#ytmp4 ${urll}`, buttonText: { displayText: '🎥 𝐕𝐈𝐃𝐄𝐎 🎥' }, type: 1 },
+{ buttonId: `#playlist ${text}`, buttonText: { displayText: '📋 𝐌𝐀𝐒 𝐑𝐄𝐒𝐔𝐋𝐓𝐀𝐃𝐎𝐒 📋' }, type: 1 }, ]    
+let texto1 = `*◉—⌈🔊 𝐘𝐎𝐔𝐓𝐔𝐁𝐄 𝐏𝐋𝐀𝐘 🔊⌋—◉*\n
+❏ 📌 *𝚃𝙸𝚃𝚄𝙻𝙾:* ${title}
+❏ 📆 *𝙿𝚄𝙱𝙻𝙸𝙲𝙰𝙳𝙾:* ${publishedTime}
+❏ ⌚ *𝙳𝚄𝚁𝙰𝙲𝙸𝙾𝙽:* ${durationH}
+❏ 👀 *𝚅𝙸𝚂𝚃𝙰𝚂:* ${viewH}
+❏ 📇 *𝙳𝙴𝚂𝙲𝚁𝙸𝙿𝙲𝙸𝙾𝙽:* ${description}
+❏ 🔗 *𝙻𝙸𝙽𝙺:* ${urll}`.trim()
+let buttonMessage = { "document": { url: "https://wa.me/5219992095479" }, "fileName": '❏ 🌿 ʀᴇᴘʀᴏᴅᴜᴄᴛᴏʀ ᴅᴇ ʏᴏᴜᴛᴜʙᴇ', "mimetype": 'application/vnd.ms-excel', "caption": texto1, "fileLength": '99999999999999', "mentions": [m.sender], "footer": wm, "buttons": buttons, "headerType": 4, contextInfo: { "mentionedJid": [m.sender], "externalAdReply": { "showAdAttribution": true, "title": `${title}`, "mediaType": 2, "previewType": "VIDEO", "thumbnail": await (await fetch(thumbnail)).buffer(), "mediaUrl": `${urll}`, "sourceUrl": `https://github.com/BrunoSobrino/TheMystic-Bot-MD` }}} 
+conn.sendMessage(m.chat, buttonMessage, { quoted: m })
 } catch {
 try {
-
-m.reply(`*[❗] 𝙻𝙰 𝙳𝙴𝚂𝙲𝙰𝚁𝙶𝙰 𝙳𝙴𝙻 𝙰𝚄𝙳𝙸𝙾 / 𝚅𝙸𝙳𝙴𝙾 𝙵𝙰𝙻𝙻𝙾, 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝙽𝙳𝙾 𝙲𝙾𝙽 𝙾𝚃𝚁𝙾 𝚂𝙴𝚁𝚅𝙸𝙳𝙾𝚁 𝙳𝙴 𝙰𝚄𝙳𝙸𝙾...*\n\n*—◉ 𝚁𝙴𝙲𝚄𝙴𝚁𝙳𝙰 𝚀𝚄𝙴 𝙿𝚄𝙴𝙳𝙴 𝚄𝚂𝙰𝚁 𝙻𝙰 𝙾𝙿𝙲𝙸𝙾𝙽 𝟹 𝙳𝙴 𝙳𝙴𝚂𝙲𝙰𝚁𝙶𝙰 𝚄𝚂𝙰𝙽𝙳𝙾 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾*\n◉ _#play3 *[texto]*_`)  
-let res = await (await fetch(`https://api.lolhuman.xyz/api/ytplay2?apikey=${lolkeysapi}&query=${text}`))    
-let json = await res.json()
-conn.sendMessage(m.chat, { audio: { url: json.result.audio }, mimetype: 'audio/mp4', fileName: json.result.title + `.mp3`}, {quoted: m})
-
-} catch  {
-m.reply('*[❗] 𝙴𝚁𝚁𝙾𝚁*')
-}}}
+let vid2 = await (await fetch(`https://api.lolhuman.xyz/api/ytsearch?apikey=${lolkeysapi}&query=${text}`)).json()
+let { videoId, title, views, published, thumbnail } = await vid2.result[0]
+const url = 'https://www.youtube.com/watch?v=' + videoId
+let ytLink = await fetch(`https://api.lolhuman.xyz/api/ytplay2?apikey=${lolkeysapi}&query=${text}`)
+let jsonn = await ytLink.json()
+let aud = await jsonn.result.audio
+let capt = `❏ 📌 *𝚃𝙸𝚃𝚄𝙻𝙾:* ${title}\n❏ 📆 *𝙿𝚄𝙱𝙻𝙸𝙲𝙰𝙳𝙾:* ${published}\n❏ 👀 *𝚅𝙸𝚂𝚃𝙰𝚂:* ${views}\n❏ 🔗 *𝙻𝙸𝙽𝙺:* ${url}`
+const buttons = [{buttonId: `#playlist ${title}`, buttonText: {displayText: '📋 𝐌𝐀𝐒 𝐑𝐄𝐒𝐔𝐋𝐓𝐀𝐃𝐎𝐒 📋'}, type: 1}]
+const buttonMessage = { image: {url: thumbnail}, caption: capt, footer: '*ᴇɴᴠɪᴀɴᴅᴏ ᴀᴜᴅɪᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...*', buttons: buttons, headerType: 4 }
+let msg = await conn.sendMessage(m.chat, buttonMessage, { quoted: m })
+conn.sendMessage(m.chat, { audio: { url: aud }, mimetype: 'audio/mp4', fileName: `${title}.mp3`}, {quoted: msg})
+} catch {  
+throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*'}}}
 handler.help = ['play', 'play2'].map(v => v + ' <pencarian>')
-handler.tags = ['general']
+handler.tags = ['downloader']
 handler.command = /^play2?$/i
-module.exports = handler
+export default handler
