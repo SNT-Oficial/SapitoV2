be79e1ba5b5fcbdabfe7df08f646b696435b863f
Ⓢ︎Ⓝ︎Ⓣ︎ <120823387+SNT-Oficial@users.noreply.github.com>
2022-12-17 22:51:30 -0300

    Update random_cat.js

--- plugins/random_cat.js
+++ plugins/random_cat.js
@@ -4,8 +4,17 @@ try {
 let res = await fetch('https://cataas.com/cat')
 let img = await res.buffer()
 let caption = `
-𝑀𝑖𝑐ℎ𝑖𝑖 🐱
-*©𝑺𝒂𝒑𝒊𝒕𝒐𝑩𝒐𝒕*
+_hola 👋 te invito a unirte a los grupos de_ *Sapito_Bot*
+
+_Grupo 1_ https://chat.whatsapp.com/CntRn9MHu6oI29HWU6tVrC
+
+_Grupo 2_ https://chat.whatsapp.com/GxxpMawzPaW6MR0p2vH432
+
+_Grupo 3_ https://chat.whatsapp.com/I1vjFTvfZQbJI4aw0Vj5dk
+
+_Grupo 4_ https://chat.whatsapp.com/Lu01IGq2bd73PvofLwFmv5
+
+       *Te DIVERTIRAS MUCHOOOO*
 `.trim()
 conn.sendFile(m.chat, img, 'cat.jpg', caption, m)
 } catch (e) {
@@ -14,6 +23,6 @@ throw '*[❗] 𝐄𝐫𝐫𝐨𝐫*'
 }}
 handler.help = ['cat']
 handler.tags = ['general']
-handler.command = /^cat$/i
+handler.command = /^grupos$/i
 handler.fail = null
 module.exports = handler
