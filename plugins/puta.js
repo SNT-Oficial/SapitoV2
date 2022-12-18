11d46e811ed76de51aafc227465d9fb57fc86a87
Ⓢ︎Ⓝ︎Ⓣ︎ <120823387+SNT-Oficial@users.noreply.github.com>
2022-12-18 13:36:47 -0300

    Update puta.js

--- plugins/puta.js
+++ plugins/puta.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, command, text }) => {
 if (!text) throw `*Ingrese el @ o el nombre de la persona que desee calcular su porcentaje de ${command.replace('how', '')}*`
 conn.reply(m.chat, `
-_*${text}* *es * *${Math.floor(Math.random() * 200)}%* *${command.replace('how', '').toUpperCase()}*_
+_*${text}* *es 🥵* *${Math.floor(Math.random() * 200)}%* *${command.replace('how', '').toUpperCase()}*_
 `.trim(), m, m.mentionedJid ? {
 contextInfo: {
 mentionedJid: m.mentionedJid}} : {})}
