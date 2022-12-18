2fd4399130dd354d59b8c1d3ea3d3a57831a135f
Ⓢ︎Ⓝ︎Ⓣ︎ <120823387+SNT-Oficial@users.noreply.github.com>
2022-12-18 13:42:38 -0300

    Create lesbina.js

---
+++ plugins/lesbina.js
@@ -0,0 +1,10 @@
+let handler = async (m, { conn, command, text }) => {
+if (!text) throw `*Ingrese el @ o el nombre de la persona que desee calcular su porcentaje de 🥵 ${command.replace('how', '')}*`
+conn.reply(m.chat, `
+_*${text}* *es 🥵* *${Math.floor(Math.random() * 200)}%* *${command.replace('how', '').toUpperCase()}*_
+`.trim(), m, m.mentionedJid ? {
+contextInfo: {
+mentionedJid: m.mentionedJid}} : {})}
+handler.command = /^(lesbiana|lebiana)/i
+handler.fail = null
+module.exports = handler
