d87fd37737873653db367489c457573f7d5ab6f8
Ⓢ︎Ⓝ︎Ⓣ︎ <120823387+SNT-Oficial@users.noreply.github.com>
2022-12-18 14:33:48 -0300

    Create estado.js

---
+++ plugins/estado.js
@@ -0,0 +1,114 @@
+let fs = require('fs')
+let handler = async (m, { conn, usedPrefix }) => {
+let pp = './Menu2.jpg'
+let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
+let menu = `
+╭══〘 ♫︎♫︎♫︎♫︎♫︎♫︎♫︎♫︎ 〙═╮
+║≡≡≡≡≡≡≡≡≡≡≡≡≡≡
+║➪ *🎆𝖧𝗈𝗅𝖺 ${taguser} 𝖢𝗈𝗆𝗈 𝖾𝗌𝗍𝖺𝗌?*
+║
+║➪ *𝐶𝑟𝑒𝑎𝑑𝑜𝑟 𝐷𝑒𝑙 𝐵𝑜𝑡:* 🇸 🇳 🇹 
+║➪ *𝑁𝑢𝑚𝑒𝑟𝑙 𝐷𝑒𝑙 𝐶𝑟𝑒𝑎𝑑𝑜𝑟 𝐷𝑒𝑙 𝐵𝑜𝑡:* +595983186566
+║
+║➪ *𝑃𝑟𝑒𝑓𝑖𝑗𝑜 𝑈𝑠𝑎𝑑𝑜:* ${usedPrefix}
+║➪ *𝑈𝑠𝑢𝑎𝑟𝑖𝑜* ${taguser}
+║➪ *𝑇𝑖𝑒𝑚𝑝𝑜 𝐴𝑐𝑡𝑖𝑣𝑜:* ????
+║≡≡≡≡≡≡≡≡≡≡≡≡≡
+╰══╡♫︎♫︎♫︎♫︎♫︎♫︎♫︎♫︎╞══╯
+┏━━━━━━━━━━━━━┓
+┃ *< 𝑪𝒐𝒎𝒂𝒏𝒅𝒐𝒔 >*
+┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡
+┣ ඬ⃟☃︎ _a_
+┣ ඬ⃟☃︎ _${usedPrefix}estado_
+┣ ඬ⃟☃︎ _${usedPrefix}dog_
+┣ ඬ⃟☃︎ _${usedPrefix}meme_
+┣ ඬ⃟☃︎ _${usedPrefix}logos_
+┣ ඬ⃟☃︎ _${usedPrefix}runtime_
+┣ ඬ⃟☃︎ _${usedPrefix}infohost_
+┣ ඬ⃟☃︎ _${usedPrefix}on welcome_
+┣ ඬ⃟☃︎ _${usedPrefix}off welcome_
+┣ ඬ⃟☃︎ _${usedPrefix}ig *[url]*_
+┣ ඬ⃟☃︎ _${usedPrefix}mediafire *[url]*_
+┣ ඬ⃟☃︎ _${usedPrefix}tiktok *[url]*_
+┣ ඬ⃟☃︎ _${usedPrefix}sticker *[url]*_
+┣ ඬ⃟☃︎ _${usedPrefix}attp *[texto]*_
+┣ ඬ⃟☃︎ _${usedPrefix}attp2 *[texto]*_
+┣ ඬ⃟☃︎ _${usedPrefix}attp3 *[texto]*_
+┣ ඬ⃟☃︎ _${usedPrefix}ttp *[texto]*_
+┣ ඬ⃟☃︎ _${usedPrefix}ttp2 *[texto]*_
+┣ ඬ⃟☃︎ _${usedPrefix}ttp3 *[texto]*_
+┣ ඬ⃟☃︎ _${usedPrefix}ttp4 *[texto]*_
+┣ ඬ⃟☃︎ _${usedPrefix}ttp5 *[texto]*_
+┣ ඬ⃟☃︎ _${usedPrefix}wikipedia *[texto]*_
+┣ ඬ⃟☃︎ _${usedPrefix}google *[texto]*_
+┣ ඬ⃟☃︎ _${usedPrefix}imagen *[texto]*_
+┣ ඬ⃟☃︎ _${usedPrefix}video *[texto]*_
+┣ ඬ⃟☃︎ _${usedPrefix}invocar *[texto]*_
+┣ ඬ⃟☃︎ _${usedPrefix}ytsearch *[texto]*_
+┣ ඬ⃟☃︎ _${usedPrefix}getaud *[url]*_
+┣ ඬ⃟☃︎ _${usedPrefix}getvid *[url]*_
+┣ ඬ⃟☃︎ _${usedPrefix}ytmp3 *[url]*_
+┣ ඬ⃟☃︎ _${usedPrefix}ytmp4 *[url]*_
+┣ ඬ⃟☃︎ _${usedPrefix}tts *[lenguaje] [texto]*_
+┣ ඬ⃟☃︎ _${usedPrefix}toimg *[sticker]*_
+┣ ඬ⃟☃︎ _${usedPrefix}sticker *[imagen]*_
+┣ ඬ⃟☃︎ _${usedPrefix}tourl *[imagen]*_
+┣ ඬ⃟☃︎ _${usedPrefix}tourl *[video]*_
+┣ ඬ⃟☃︎ _${usedPrefix}tourl *[audio]*_
+┣ ඬ⃟☃︎ _${usedPrefix}fat *[nota de voz]*_
+┣ ඬ⃟☃︎ _${usedPrefix}bass *[nota de voz]*_
+┣ ඬ⃟☃︎ _${usedPrefix}blown *[nota de voz]*_
+┣ ඬ⃟☃︎ _${usedPrefix}deep *[nota de voz]*_
+┣ ඬ⃟☃︎ _${usedPrefix}fast *[nota de voz]*_
+┣ ඬ⃟☃︎ _${usedPrefix}robot *[nota de voz]*_
+┣ ඬ⃟☃︎ _${usedPrefix}slow *[nota de voz]*_
+┣ ඬ⃟☃︎ _${usedPrefix}tupai *[nota de voz]*_
+┣ ඬ⃟☃︎ _${usedPrefix}vibra *[nota de voz]*_
+┣ ඬ⃟☃︎ _${usedPrefix}nightcore *[nota de voz]*_
+┣ ඬ⃟☃︎ _${usedPrefix}earrape *[nota de voz]*_
+┣ ඬ⃟☃︎ _${usedPrefix}reverse *[nota de voz]*_
+┣ ඬ⃟☃︎ _${usedPrefix}smooth *[nota de voz]*_
+   *𝐽𝑢𝑒𝑔𝑜𝑠*
+┣ ඬ⃟☃︎ _${usedPrefix}puta_
+┣ ඬ⃟☃︎ _${usedPrefix}perra_
+┣ ඬ⃟☃︎ _${usedPrefix}insano_
+┣ ඬ⃟☃︎ _${usedPrefix}gay_
+┣ ඬ⃟☃︎ _${usedPrefix}lesbiana_
+┣ ඬ⃟☃︎ _${usedPrefix}doxear
+┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡
+┃ *< 𝑂𝑤𝑛𝑒𝑟 >*
+┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡
+┣ ඬ⃟シ︎ _${usedPrefix}update_
+┣ ඬ⃟シ︎ _${usedPrefix}banchat_
+┣ ඬ⃟シ︎ _${usedPrefix}unbanchat_
+┗━━━━━━━━━━━━━┛
+`.trim()
+let buttons = [
+{ buttonId: '#owner', buttonText: { displayText: '🐸 CREADOR 🐸' }, type: 1 },
+{ buttonId: '#bot cogemos?', buttonText: { displayText: '🤑 COGEMOS? 🤑' }, type: 1 },
+{ buttonId: '#logo', buttonText: { displayText: '🐎 LOGOS 🐎' }, type: 1 }]
+let buttonMessage = {
+image: fs.readFileSync('./Menu2.jpg'),
+caption: menu.trim(),
+mentions: [m.sender],
+footer: `*${wm}*`,
+buttons: buttons,
+headerType: 4,
+contextInfo: {
+mentionedJid: [m.sender],
+externalAdReply: {
+showAdAttribution: true,
+mediaType: 'VIDEO',
+mediaUrl: null,
+title: '👑 𝑺𝒊𝒎𝒑𝒍𝒆 𝑺𝒂𝒑𝒊𝒕𝒐 𝑩𝒐𝒕 👑',
+body: null,
+thumbnail: fs.readFileSync('./src/logo.png'),
+sourceUrl: `https://github.com/Micasatio/SAPITO-`
+}}}
+conn.sendMessage(m.chat, buttonMessage, { quoted: m })
+}
+handler.help = ['menu', 'help', '?']
+handler.tags = ['general']
+handler.command = /^(estado|stado|bot?|activo|condition|status|sii|\?)$/i
+handler.fail = null
+module.exports = handler
